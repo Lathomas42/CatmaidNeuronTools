7ea820a5719b61e854480b6ce51ccc74840ae87e
Lathomas42 <thomas.lo@husky.neu.edu>
2014-07-29 11:47:31 -0400

    Parser

---
+++ ParseNeuronDistDict.py
@@ -0,0 +1,26 @@
+import numpy
+import catmaid
+
+def parseDistDict(ndd):
+	
+    nronsOG = ndd.keys()
+    nrons = nronsOG
+    intnrons = []
+    for nr in nronsOG:
+	if(nr in ndd.keys()):
+	    newrons = ndd[nr].keys()
+	    for new in newrons:
+	        if new not in nrons:
+                    nrons.append(new)
+    denadj = numpy.zeros([len(nrons),len(nrons)])
+    axadj = numpy.zeros([len(nrons),len(nrons)])
+
+    for nr in ndd.keys():
+	inda = nrons.index(nr)
+	for new in ndd[nr].keys():
+            indb = nrons.index(new)
+	    axadj[inda][indb] = ndd[nr][new]['ax1']
+	    denadj[inda][indb] = ndd[nr][new]['den1']
+	    axadj[indb][inda] = ndd[nr][new]['ax2']
+	    denadj[indb][inda] = ndd[nr][new]['den2']
+    return denadj, axadj
